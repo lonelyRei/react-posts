bc9c5ae32e3b6cc6509e0d418c4bfb6140902f68
lonelyRei <shepovalovda@gmail.com>
2023-03-01 22:28:57 +0400

    Авторизация и все, что с ней связано

--- src/API/PostService.ts
+++ src/API/PostService.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 import { ICommentItem, IPostsListItem } from '../types'
 import { generateRandomTheme } from '../utils/themes'
-import { log } from 'util'
 export default class PostService {
     // URL для получения всех постов
     private static postsUrl: string = 'https://jsonplaceholder.typicode.com/posts'
@@ -29,72 +28,65 @@ export default class PostService {
     // Статичная функция для получения всех постов
     public static getAllPosts(limit: number = 10, page: number = 1): Promise<[IPostsListItem[], number]> {
         return axios
-            .get(PostService.postsUrl, {
+            .get<postsResponse[]>(PostService.postsUrl, {
                 params: {
                     _limit: limit, // Максимум постов в ответе
                     _page: page, // Номер страницы
                 },
             })
-            .then(
-                (response: AxiosResponse<any>) => {
-                    // Вытаскиваем данные
-                    const data: postsResponse[] = response.data
-                    // Создаем список постов (пока что пустой)
-                    const correctData: IPostsListItem[] = []
-                    // Наполняем список корректными постами
-                    data.forEach((post: postsResponse) => {
-                        // Формируем корректный пост и пушим его в массив
-                        // тему генерируем случайно (в теле ответа ее нет)
-                        correctData.push(this.getCorrectPost(post, generateRandomTheme()))
-                    })
-                    // Возвращаем массив корректных постом и количество постов, доступных для отрисовки
-                    return [correctData, Number(response.headers['x-total-count'])]
-                },
-                (e: any) => {
-                    // Если что-то пошло не так, то просто генерируем ошибку
-                    throw new Error(e.message)
-                }
-            )
+            .then((response: AxiosResponse<postsResponse[]>) => {
+                // Вытаскиваем данные
+                const data: postsResponse[] = response.data
+                // Создаем список постов (пока что пустой)
+                const correctData: IPostsListItem[] = []
+                // Наполняем список корректными постами
+                data.forEach((post: postsResponse) => {
+                    // Формируем корректный пост и пушим его в массив
+                    // тему генерируем случайно (в теле ответа ее нет)
+                    correctData.push(this.getCorrectPost(post, generateRandomTheme()))
+                })
+                // Возвращаем массив корректных постом и количество постов, доступных для отрисовки
+                return [correctData, Number(response.headers['x-total-count'])]
+            })
+            .catch((error) => {
+                return error
+            })
     }
 
     // Возвращает пост по id
     public static getPost(id: string): Promise<IPostsListItem> {
-        return axios.get(PostService.postsUrl + `/${id}`).then(
-            (response: AxiosResponse<any>) => {
+        return axios
+            .get<postsResponse>(PostService.postsUrl + `/${id}`)
+            .then((response: AxiosResponse<postsResponse>) => {
                 // Вытаскивает данные
                 const data: postsResponse = response.data
                 // Формируем корректный пост, тему генерируем случайно (в теле ответа ее нет)
                 return this.getCorrectPost(data, generateRandomTheme())
-            },
-            (e: any) => {
-                // Выкидываем ошибку, если что-то пошло не так
-                throw new Error(e.message)
-            }
-        )
+            })
+            .catch((error) => {
+                return error
+            })
     }
 
     // Возращает список комментариев, соответствующих id поста
-    public static getCommentsById(postId: string) {
+    public static getCommentsById(postId: string): Promise<ICommentItem[]> {
         return axios
-            .get(PostService.commentsUrl, {
+            .get<commentsResponse[]>(PostService.commentsUrl, {
                 params: {
                     postId: postId,
                 },
             })
-            .then(
-                (response: AxiosResponse<any>) => {
-                    const data: commentsResponse[] = response.data
-                    const result: ICommentItem[] = []
-                    data.forEach((comment: commentsResponse): void => {
-                        result.push(this.getCorrectComment(comment))
-                    })
-                    return result
-                },
-                (e: any) => {
-                    // Выкидываем ошибку, если что-то пошло не так
-                    throw new Error(e.message)
-                }
-            )
+            .then((response: AxiosResponse<commentsResponse[]>) => {
+                const data: commentsResponse[] = response.data
+                const result: ICommentItem[] = []
+                data.forEach((comment: commentsResponse): void => {
+                    result.push(this.getCorrectComment(comment))
+                })
+                return result
+            })
+            .catch((error) => {
+                return error
+            })
     }
 }
 
